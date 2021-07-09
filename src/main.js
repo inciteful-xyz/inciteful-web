9c7b3b5fdd1be92ec5b5a640871cdbba80172089
Michael Weishuhnn <weishuhn@gmail.com>
2021-07-09 16:19:11 +0100

    Working through the docs

--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import store from './store'
 
 import * as Sentry from '@sentry/vue'
 import { Integrations } from '@sentry/tracing'
-import './styles/style.scss'
-import api from './utils/api'
-import widgets from './widgets'
-import navigation from './navigation'
-import options from './utils/options'
+// import './styles/style.scss'
+// import api from './utils/api'
+// import widgets from './widgets'
+// import navigation from './navigation'
+// import options from './utils/options'
 import VueTour from 'vue-tour'
 
 import VueSocialSharing from 'vue-social-sharing'
@@ -17,10 +17,9 @@ import VueSocialSharing from 'vue-social-sharing'
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     Vue: Vue,
-    dsn: 'https://e0f9638a22234f65b69a22a10537ab95@o415910.ingest.sentry.io/5512736',
-    integrations: [
-      new Integrations.BrowserTracing()
-    ],
+    dsn:
+      'https://e0f9638a22234f65b69a22a10537ab95@o415910.ingest.sentry.io/5512736',
+    integrations: [new Integrations.BrowserTracing()],
     release: 'inciteful-js@' + process.env.__COMMIT_HASH__,
     environment: process.env.NODE_ENV,
     tracingOptions: {
@@ -42,9 +41,9 @@ require('vue-tour/dist/vue-tour.css')
 Vue.use(VueTour)
 Vue.use(VueSocialSharing)
 
-export default {
-  widgets,
-  navigation,
-  api,
-  options
-}
+// export default {
+//   widgets,
+//   navigation,
+//   api,
+//   options
+// }
