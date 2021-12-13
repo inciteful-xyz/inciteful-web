a56bc059253e1295da1cab2d2811c09516c60bad
Michael Weishuhnn <weishuhn@gmail.com>
2021-12-13 16:48:54 +0000

    making my own autocomplete.

--- src/components/Autosuggest.vue
+++ src/components/Autosuggest.vue
@@ -1,30 +1,15 @@
 <template>
   <div class="text-gray-800 relative">
-    <!-- <vue-autosuggest
-      ref="autocomplete"
+    <input
+      class="w-full px-4 py-2 border border-gray-300 rounded-md leading-5
+    bg-white transition duration-150 ease-in-out focus:outline-none focus:border-blue-300"
+      ref="searchBox"
+      type="text"
+      placeholder="Paper title, DOI, PubMed URL, or arXiv URL"
       v-model="query"
-      :suggestions="suggestions"
-      :inputProps="inputProps"
-      :getSuggestionValue="getSuggestionValue"
-      :sectionConfigs="sectionConfigs"
-      @selected="selectHandler"
-      @input="fetchResults"
-    >
-      <template v-slot="{ suggestion }">
-        <div class="pb-1 text-sm">
-          {{ suggestion.item.title }}
-        </div>
-        <div class="text-xs">
-          <Authors :authors="suggestion.item.author" /> ({{
-            suggestion.item.published_year
-          }}) -
-          {{ format(suggestion.item.num_cited_by) }}
-          citations
-        </div>
-      </template>
-    </vue-autosuggest> -->
+    />
     <div
-      v-if="!selectIsValid"
+      v-if="results"
       class="
         origin-top-right
         absolute
@@ -37,20 +22,27 @@
         rounded-md
         bg-white
         p-0
+        m-0
         overflow-y-scroll
         max-h-96;
       "
     >
-      <div class="rounded-md bg-white shadow-xs p-2">
-        <div
-          role="menu"
-          aria-orientation="vertical"
-          aria-labelledby="options-menu"
+      test
+      <ul class="list-none p-0 m-0">
+        <li
+          class="cursor-pointer p-2 border-t border-gray-200 hover:bg-gray-200"
+          v-for="result in results"
+          :key="result.id"
+          @click="sendSelect([result.id])"
         >
-          Invalid search: Check your spelling, enter a DOI, or another paper
-          identifier
-        </div>
-      </div>
+          <div class="pb-1 text-sm">{{ result.title }} title</div>
+          <div class="text-xs">
+            <Authors :authors="result.author" /> ({{ result.published_year }}) -
+            {{ format(result.num_cited_by) }}
+            citations
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -59,8 +51,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-ignore */
 
 import { defineComponent } from 'vue'
-// @ts-ignore
-// import { VueAutosuggest } from 'vue-autosuggest'
 import api from '../utils/api'
 // import Authors from './Authors.vue'
 import numeral from 'numeral'
@@ -90,24 +80,10 @@ export default defineComponent({
   data () {
     return {
       query: '',
-      results: [],
+      results: undefined as Paper[] | undefined,
       timeout: null as number | null,
       selected: null as PaperID | null,
       debounceMilliseconds: 200,
-      sectionConfigs: {
-        default: {
-          limit: 10,
-          // @ts-ignore
-          onSelected: this.selectHandler
-        }
-      },
-      inputProps: {
-        id: 'autosuggest__input',
-        placeholder: 'Paper title, DOI, PubMed URL, or arXiv URL',
-        class: 'form-control',
-        name: 'hello'
-      },
-      suggestions: [] as { name: string; data: Paper[] }[],
       selectIsValid: true
     }
   },
@@ -120,6 +96,26 @@ export default defineComponent({
           this.query = this.getPaperValue(data)
         }
       })
+    }
+  },
+  watch: {
+    query (newVal) {
+      const query = newVal
+      this.selectIsValid = true
+
+      if (this.timeout) {
+        clearTimeout(this.timeout)
+      }
+
+      this.timeout = setTimeout(() => {
+        this.selected = null
+
+        api.searchPapers(query).then(papers => {
+          if (papers && papers.length > 0) {
+            this.results = papers
+          }
+        })
+      }, this.debounceMilliseconds)
     }
   },
   methods: {
@@ -138,40 +134,8 @@ export default defineComponent({
     format (val: number) {
       return numeral(val).format('0,0.[000000]')
     },
-    fetchResults (): void {
-      const query = this.query
-      this.selectIsValid = true
-
-      if (this.timeout) {
-        clearTimeout(this.timeout)
-      }
-
-      this.timeout = setTimeout(() => {
-        this.selected = null
-
-        api.searchPapers(query).then(papers => {
-          if (papers && papers) {
-            this.suggestions = [
-              {
-                name: 'default',
-                data: papers
-              }
-            ]
-          }
-        })
-      }, this.debounceMilliseconds)
-    },
-    getSuggestionValue (suggestion: { item: Paper }): string {
-      return this.getPaperValue(suggestion.item)
-    },
     getPaperValue (paper: Paper): string {
       return `${paper.title} (${paper.id})`
-    },
-    selectHandler (suggestion: { item: { id: string } }): void {
-      if (suggestion) {
-        this.selected = suggestion.item.id
-        this.sendSelect([suggestion.item.id])
-      }
     },
     sendSelect (ids: PaperID[]) {
       api.getPaperIds(ids).then(ids => {
@@ -183,34 +147,10 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-#autosuggest__input {
-  @apply w-full px-4 py-2 border border-gray-300 rounded-md leading-5 bg-white transition duration-150 ease-in-out;
-}
-
-#autosuggest {
-  @apply w-full;
-}
-
-#autosuggest__input:focus {
-  @apply outline-none border-blue-300;
-}
 // placeholder-gray-500  sm:text-sm  placeholder-gray-400;
 
 #autosuggest__input.autosuggest__input-open {
   @apply rounded-b;
-}
-
-.autosuggest__results-container {
-  @apply relative w-full;
-}
-
-.autosuggest__results {
-  @apply absolute z-50 w-full;
-  @apply rounded-b border rounded-tr-none rounded-tl-none border-gray-300 rounded-md bg-white p-0 overflow-y-scroll max-h-96;
-}
-
-.autosuggest__results ul {
-  @apply list-none pl-0 m-0;
 }
 
 .autosuggest__results .autosuggest__results-item {
