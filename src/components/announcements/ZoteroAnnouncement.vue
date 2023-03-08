4b1e7aff0fb6f25164f7c1683933de6b667877d5
Michael Weishuhn <weishuhn@gmail.com>
2023-03-08 13:55:31 +0000

    Updated plugin announcement.

--- src/components/announcements/ZoteroAnnouncement.vue
+++ src/components/announcements/ZoteroAnnouncement.vue
@@ -2,8 +2,10 @@
   <AnnouncementBanner>
     <template v-slot:title>
       Did you know you can search directly from Zotero? Check out our new <a class="underline"
-        href="https://github.com/inciteful-xyz/inciteful-zotero-plugin">Zotero
-        plugin</a>!
+        href="https://github.com/inciteful-xyz/inciteful-zotero-plugin" target="_blank">Zotero
+        plugin</a> and see the <a class="underline" href="https://twitter.com/Inciteful_xyz/status/1633447883994193920"
+        target="_blank">Twitter
+        thread</a> on how to use it.
     </template>
   </AnnouncementBanner>
 </template>
