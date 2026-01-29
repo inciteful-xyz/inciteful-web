b6a8d101fd3900e1811f8486eb0dc56c0637e91f
Michael Weishuhn <weishuhn@gmail.com>
2026-01-29 10:31:09 -0700

    Type fixes

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,7 +1,7 @@
-/* eslint-disable */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+  const component: DefineComponent<{}, {}, unknown>
   export default component
 }
 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,14 +4,12 @@ declare global {
   const __COMMIT_HASH__: string
 }
 
-interface ImportMetaEnv {
-  readonly VITE_CLIENT_API_URL: string
-  readonly VITE_SHOW_LOGIN: string
-  readonly VITE_PROJECT_ID: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
+declare module 'vite/client' {
+  interface ImportMetaEnv {
+    readonly VITE_CLIENT_API_URL: string
+    readonly VITE_SHOW_LOGIN: string
+    readonly VITE_PROJECT_ID: string
+  }
 }
 
 export {}
